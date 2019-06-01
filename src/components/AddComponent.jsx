7c55ff340bf5b5d0a864e0cbd23fdfdf1aef8deb
Aditya <aditya-tyagi@outlook.com>
2019-06-01 10:41:51 -0500

    Lifting the state up

---
+++ src/components/AddComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class AddComponent extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default AddComponent;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,52 +1,23 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 2 },
-      { id: 2, value: 34 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counter[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    console.log("Event Handler Called", counterId);
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
+ 
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
             selected={true}
             counter={counter}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
           >
             <h4>Counter #{counter.id}</h4>
           </Counter>
